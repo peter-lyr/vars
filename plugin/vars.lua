f06a901fbc713bd6822e947bceb4d509400fe937
peter-lyr <llydrp.ldp@gmail.com>
2024-09-15 23:08:11 +0800

    Config = vim.fn.stdpath 'config'

--- plugin/vars.lua
+++ plugin/vars.lua
@@ -1,5 +1,7 @@
-Home = vim.fn.expand '$HOME'
-Org = Home .. '\\org'
+Home   = vim.fn.expand '$HOME'
+Config = vim.fn.stdpath 'config'
+Org    = Home .. '\\org'
+Dp     = Home .. '\\dp'
 
-Name = 'peter-lyr'
-Email = 'llydrp.ldp@gmail.com'
+Name   = 'peter-lyr'
+Email  = 'llydrp.ldp@gmail.com'
